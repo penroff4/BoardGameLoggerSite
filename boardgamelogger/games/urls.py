d0aadb93c4efe0be0add79fa283575bdedff54d7
penroff4 <penroff4@gmail.com>
2021-12-14 13:02:21 -0800

    gameVoting initial commit pre vote model

--- boardgamelogger/games/urls.py
+++ boardgamelogger/games/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'games'
+
 urlpatterns = [
         # ex: /games/
         path('', views.index, name='index'),
 
         # ex: /games/1/
         path('<int:game_id>/', views.gameDetail, name='detail'),
+
+        # ex: /games/1/vote
+        path('<int:game_id>/vote/', views.vote,name='vote'),
         ]
